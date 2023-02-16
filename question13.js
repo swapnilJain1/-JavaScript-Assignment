22949c94d5b0b135f30efebabe35f1a951cd2856
Swapnil Jain <119680399+swapnilJain1@users.noreply.github.com>
2023-02-16 12:35:34 +0530

    Update question13.js

--- question13.js
+++ question13.js
@@ -1,3 +1,9 @@
 // 13. Get user input using prompt(“Enter your age:”). If user is 18 or older , give feedback:'You are old enough to drive' but if not 18 give another feedback stating to wait for the number of years he needs to turn 18.
 
-prompt()
+const is18 = prompt("Enter your age:")
+if(is18>=18){
+    console.log("Good for driving")
+}
+else{
+    console.log("You are not 18 years old");
+}
